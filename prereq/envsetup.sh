a9ba857a90274a7a7f767bf2d06449d6bd8330ac
Naren C S <narencs179@gmail.com>
2025-02-07 03:20:23 +0530

    Integrated current envsetup

--- prereq/envsetup.sh
+++ prereq/envsetup.sh
@@ -13,8 +13,28 @@ mkdir -p "${ENVBASEDIR}"
 
 # We fix a specific version of Python by hand-installing it.
 bash "${BIN_DIR}/install-python3.11.11" "${BIN_DIR}"
+bash "${BIN_DIR}/install-python3.9.12" "${BIN_DIR}"
+bash "${BIN_DIR}/install-python3.6.15" "${BIN_DIR}"
 
 PY="${BIN_DIR}/python-3.11.11/bin/python3"
+PY2="${BIN_DIR}/python-3.9.12/bin/python3"
+PY3="${BIN_DIR}/python-3.6.15/bin/python3"
+
+(
+    cd "$BIN_DIR"
+    git clone https://github.com/GfellerLab/MixMHCpred.git
+)
+
+# Download TransPHLA and modifiying it to fix some bugs and
+# improve user convenience.
+(
+    cd "$BIN_DIR" 
+    # We use the specific commit of TransPHLA that was most recent when
+    # we were benchmarking.
+    git clone https://github.com/a96123155/TransPHLA-AOMP.git
+)
+cp "${PREREQ_DIR}/transphla-modfiles/"*.py "${BIN_DIR}/TransPHLA-AOMP/TransPHLA-AOMP"
+
 ENVDIR="${ENVBASEDIR}/deephlaffy"
 ENVACTV="${ENVDIR}/bin/activate"
 ENVPY="${ENVDIR}/bin/python3"
@@ -30,24 +50,13 @@ ENVPY="${ENVDIR}/bin/python3"
 TPHLA_ENVDIR="${ENVBASEDIR}/transphla"
 TPHLA_ENVACTV="${TPHLA_ENVDIR}/bin/activate"
 TPHLA_ENVPY="${TPHLA_ENVDIR}/bin/python3"
-"$PY" -m venv "$TPHLA_ENVDIR"
+"$PY2" -m venv "$TPHLA_ENVDIR"
 
 # The subshell here serves the same purpose as earlier and prevents
 # spillover of the environment onto the next steps of the script.
-# 
-# We use the specific commit of TransPHLA that was most recent when
-# we were benchmarking.
 (
     source "$TPHLA_ENVACTV"
-    cd "${TPHLA_ENVDIR}/bin"
-    git clone https://github.com/a96123155/TransPHLA-AOMP.git
-    cd TransPHLA-AOMP/TransPHLA-AOMP
-
-    # Fix indentation error
-    sed -i 's/        log = Logger(errLogPath)/    log = Logger(errLogPath)/' pHLAIformer.py
-
-    # Fix bug in iteration variable (hla->hla_seq)
-    sed -i 's/    if not (pep.isalpha() and hla.isalpha()):/    if not (pep.isalpha() and hla_seq.isalpha()):/' pHLAIformer.py
+    "${TPHLA_ENVPY}" -m pip install -r "${PREREQ_DIR}/transphla-req.txt"
 )
 
 
@@ -60,5 +69,17 @@ MHCFL_ENVPY="${MHCFL_ENVDIR}/bin/python3"
     "$MHCFL_ENVPY" -m pip install mhcflurry==2.1.2
     "$MHCFL_ENVPY" -m pip install numpy==1.23.5
     mhcflurry-downloads fetch
+    deactivate
+)
+
+
+MMP_ENVDIR="${ENVBASEDIR}/mixmhcpred"
+MMP_ENVACTV="${MMP_ENVDIR}/bin/activate"
+MMP_ENVPY="${MMP_ENVDIR}/bin/python3"
+"$PY" -m venv "$MMP_ENVDIR"
+
+(
+    source "$MMP_ENVACTV"
+    "$MMP_ENVPY" -m pip install -r "${BIN_DIR}/MixMHCpred/code/setup_pythonLibrary.txt"
     deactivate
 )
